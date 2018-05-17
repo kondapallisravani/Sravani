9f8166a6204659af3a2d90ae9910814c8baaeebd
368731 <368731@PC382446.cts.com>
2018-05-17 15:50:28 +0530

    added set up files

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@
 
 sudo docker stop $(sudo docker ps -aq)
 sudo docker rm $(sudo docker ps -aq)
-docker-compose up
+docker-compose up -d petclinic 
+docker-compose up -d zalenium 
+until curl -s --head  --request GET http://172.18.0.22:8080/petclinic/ | grep "200" > /dev/null; do >&2 echo "App is unavailable - sleeping" sleep 1 done >&2 echo "App is up - executing command" exec $cmd
+docker-compose up -d cucumberscript 
