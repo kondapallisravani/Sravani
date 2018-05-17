f490def99a5c73eab3c3ba2b55a7ae07267c7a97
368731 <368731@PC382446.cts.com>
2018-05-17 14:57:15 +0530

    added set up files

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # wait-for-site.sh
 
-
-docker-compose up -d
-sh ./waitforsite.sh
-docker run -it --net=downloads_mysubnet123 ganeshtidke0901docker/selenium-bdd-poc  mvn verify -Dweb.app.docker.ip=172.18.0.22 -Dhub.host.ip=192.168.40.192
- 
+sudo docker stop $(sudo docker ps -aq)
+sudo docker rm $(sudo docker ps -aq)
+docker-compose up
