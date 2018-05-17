ea7680709d15c1443fe00404deb65d1494858011
368731 <368731@PC382446.cts.com>
2018-05-17 16:01:37 +0530

    added set up files

--- start.sh
+++ start.sh
@@ -5,5 +5,4 @@ docker stop $(sudo docker ps -aq)
 docker rm $(sudo docker ps -aq)
 docker-compose up -d petclinic 
 docker-compose up -d zalenium 
-sh ./waitforsite.sh
 docker-compose up cucumberscript 
