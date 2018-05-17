15ed5e6fe64fb775428a737e9ef73c998c2dce61
368731 <368731@PC382446.cts.com>
2018-05-17 15:59:18 +0530

    added set up files

--- waitforsite.sh
+++ waitforsite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # wait-for-site.sh
 
-#until curl -s --head  --request GET http://172.18.0.22:8080/petclinic/ | grep "200" > /dev/null; do >&2 echo "App is unavailable - sleeping" sleep 1 done >&2 echo "App is up - executing command" exec $cmd
+until curl -s --head  --request GET http://172.18.0.22:8080/petclinic/ | grep "200" > /dev/null; do >&2 echo "App is unavailable - sleeping" sleep 1 done >&2 echo "App is up - executing command" exec $cmd
 
