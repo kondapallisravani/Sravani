604467d2c36c816645ec42814074871dcd17b9c4
368731 <368731@PC382446.cts.com>
2018-05-24 16:24:35 +0530

    Merge origin/master

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 
 docker stop $(sudo docker ps -aq)
 docker rm $(sudo docker ps -aq)
+docker network rm $(sudo docker network ls)
 docker-compose up -d petclinic 
 docker-compose up -d zalenium
 git clone -b master --single-branch 'https://368731:gvt0901!@code.cognizant.com/368731/cucumbercraft.git'
@@ -10,6 +11,6 @@ cd cucumbercraft
 docker rmi cucumbercraft
 docker  build --tag cucumbercraft:latest .
 docker-compose up cucumbercraft
-echo "please click below for aggregated result http://localhost:3389/craftreport/cucumber-results-agg-test-results.html"
-echo "please click below for overview result http://localhost:3389/craftreport/cucumber-results-feature-overview.html.html"
-echo "please click below for test result in PDF http://localhost:3389/craftreport/cucumber-results-test-results.pdf"
+echo "please click below for aggregated result http://192.168.40.192:3389/craftreport/cucumber-results-agg-test-results.html"
+echo "please click below for overview result http://192.168.40.192:3389/craftreport/cucumber-results-feature-overview.html.html"
+echo "please click below for test result in PDF http://192.168.40.192:3389/craftreport/cucumber-results-test-results.pdf"
