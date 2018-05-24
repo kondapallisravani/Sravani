a021ec806372e05690307ee144a854199951204d
368731 <368731@PC382446.cts.com>
2018-05-24 10:55:31 +0530

    Merge origin/master

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ docker stop $(sudo docker ps -aq)
 docker rm $(sudo docker ps -aq)
 docker-compose up -d petclinic 
 docker-compose up -d zalenium
+git clone -b master --single-branch 'https://368731:gvt0901!@code.cognizant.com/368731/cucumbercraft.git'
+cd cucumbercraft
 docker  build --tag cucumbercraft:latest .
 docker-compose up cucumbercraft 
