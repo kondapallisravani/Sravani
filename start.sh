fd5f42ddd32fcc6a5df7d8ae582fd09f4b2e6563
368731 <368731@PC382446.cts.com>
2018-05-24 11:29:57 +0530

    Merge origin/master

--- start.sh
+++ start.sh
@@ -7,5 +7,9 @@ docker-compose up -d petclinic
 docker-compose up -d zalenium
 git clone -b master --single-branch 'https://368731:gvt0901!@code.cognizant.com/368731/cucumbercraft.git'
 cd cucumbercraft
+docker rmi cucumbercraft
 docker  build --tag cucumbercraft:latest .
-docker-compose up cucumbercraft 
+docker-compose up cucumbercraft
+echo "please click below for aggregated result http://localhost:3389/craftreport/cucumber-results-agg-test-results.html"
+echo "please click below for overview result http://localhost:3389/craftreport/cucumber-results-feature-overview.html.html"
+echo "please click below for test result in PDF http://localhost:3389/craftreport/cucumber-results-test-results.pdf"
