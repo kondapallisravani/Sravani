6efe1f70ddec5be4ff0afef2edd58a8adac62a42
root <code.cognizant.com>
2018-05-29 16:06:26 +0530

    changes

--- start.sh
+++ start.sh
@@ -11,12 +11,13 @@ cd cucumbercraft-2.0
 docker rmi cucumbercraft
 docker  build --tag cucumbercraft:latest .
 docker-compose up cucumbercraft
-sudo rm /home/qpaas/webapps/craftreport/cucumber-results-agg-test-results.html
-sudo rm /home/qpaas/webapps/craftreport/cucumber-results-feature-overview.html
-sudo rm /home/qpaas/webapps/craftreport/cucumber-results-test-results.pdf
+sudo rm -rfd /home/qpaas/webapps/craftreport/allure-results
+sudo rm -rfd /home/qpaas/webapps/craftreport/css
+sudo rm -rfd /home/qpaas/webapps/craftreport/images
+sudo rm -rfd /home/qpaas/webapps/craftreport/project-reports.html
 sudo rm -rfd /home/qpaas/craftresult
-docker cp cucumbercraft:/cucumbercraft/target/site/.  /home/qpaas/craftresult
-sudo cp /home/qpaas/craftresult/* /home/qpaas/webapps/craftreport/
+docker cp cucumbercraft:/cucumbercraft-2.0/target/site/.  /home/qpaas/craftresult
+sudo cp -fr /home/qpaas/craftresult/* /home/qpaas/webapps/craftreport/
 echo "End of Execution"
 echo "please click below for aggregated result http://192.168.40.192:3389/craftreport/allure-results/index.html"
 
